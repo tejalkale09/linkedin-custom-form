27043949a4e76427b84f2a2944f4e4dba00c06c5
tejalkale09 <tejalkale86@gmail.com>
2026-02-17 17:00:19 +0530

    Create route.ts

---
+++ app/api/pexels/route.ts
@@ -0,0 +1,27 @@
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url)
+  const query = searchParams.get("query")
+
+  if (!query) {
+    return Response.json({ error: "Query is required" }, { status: 400 })
+  }
+
+  const res = await fetch(
+    https://api.pexels.com/v1/search?query=${query}&per_page=9,
+    {
+      headers: {
+        Authorization: process.env.PEXELS_API_KEY as string,
+      },
+    }
+  )
+
+  if (!res.ok) {
+    return Response.json(
+      { error: "Failed to fetch from Pexels" },
+      { status: res.status }
+    )
+  }
+
+  const data = await res.json()
+  return Response.json(data)
+}
